b547643ef400cc7e29e4e18e49b5d06d51695a85
PHopson <peter.c.hopson@gmail.com>
2022-03-10 22:37:19 -0500

    DB model/controller changes

---
+++ app/Models/Message.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = ['user_id_a','user_id_b','message'];
+    use HasFactory;
+}
